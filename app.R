f2d15a90a18748e5e6e5a09b678baaad7efcf608
Kang Hui Yun <huiyun.kang.2016@sis.smu.edu.sg>
2020-04-10 12:42:04 +0800

    Add in GCI Ranking (Bump Chart)

--- app.R
+++ app.R
@@ -3,14 +3,26 @@
 
 
 ## app.R ##
-library(shinydashboard)
-library(ggplot2)
+library(readr)
 library(plyr)
 library(dplyr)
+library(forcats)
+library(randomcoloR)
+library(plotly)
+library(crosstalk)
+library(shinydashboard)
 
-#data preparation
+# Global Competitiveness Index
+gci <- read_csv("data/cleaned/cleaned_GCI.csv", col_types = "ffif")
 
-data_enrollment_institute <- read.csv("Cleaned/cleaned_enrollment-by-institute.csv")
+gci$Series <- fct_relevel(gci$Series, "5th pillar: Higher education and training", after = 0)
+
+gci_top_10 <- gci %>%
+    filter(Rank %in% (1:10)) %>%
+    arrange(Series, desc(Year), Rank)
+
+# Enrolment
+data_enrollment_institute <- read.csv("data/cleaned/cleaned_enrollment-by-institute.csv")
 data_enrollment_institute <- filter(data_enrollment_institute, Enrollment > 0)
 data_poly_enrollment_institute <- filter(data_enrollment_institute, Institute_Type == "Polytechnic")
 data_uni_enrollment_institute <- filter(data_enrollment_institute, Institute_Type == "University")
@@ -31,7 +43,6 @@ ui <- dashboardPage(
     dashboardBody(
         # Boxes need to be put in a row (or column)
         tabItems(
-            
             tabItem(tabName = "Intro",
                 fluidRow(
                     box(plotOutput("plot1", height = 250)),
@@ -43,7 +54,12 @@ ui <- dashboardPage(
                 )
             ),
             tabItem(tabName = "Ranking",
-                    h2("Singapore has increased its Higher Education and Training progressively and has maintained its top position since 2015-16.")    
+                    h2("Singapore has progressively upped its ranking over the years and clinched the first position for the 5th pillar (Higher Education and Training) of GCI since 2015-16."),
+                    selectInput("gci_category",
+                                "Category",
+                                choices = levels(gci_top_10$Series),
+                                width = "400px"),
+                    plotlyOutput("gci_ranking")
             ),
             tabItem(tabName = "Enrollment",
                 h2("Singapore's Higher Education Enrollment"),
@@ -95,7 +111,43 @@ server <- function(input, output) {
     set.seed(122)
     histdata <- rnorm(500)
     
-    
+    # Global Competitiveness Index (Bump Chart)
+    output$gci_ranking <- renderPlotly({
+        gci_top_10_category <- gci_top_10 %>%
+            filter(Series == input$gci_category) %>%
+            droplevels()
+        
+        if ("Singapore" %in% gci_top_10_category$Country) {
+            gci_top_10_category %>%
+                plot_ly(x = ~fct_rev(Year), y = ~Rank,
+                        type = "scatter", mode = "lines+markers",
+                        line = list(width = 3), marker = list(size = 10),
+                        color = I("darkgray"),
+                        hoverinfo = "text",
+                        text = ~paste("Country:", Country, "<br>Year:", Year, "<br>Rank:", Rank),
+                        transforms = list(
+                            list(type = "groupby",
+                                 groups = ~Country,
+                                 styles = list(
+                                     list(target = "Singapore",
+                                          value = list(line = list(color = "red"),
+                                                       marker = list(color = "red")))
+                                 )))
+                ) %>%
+                layout(yaxis = list(autorange = "reversed", tickmode = "linear", showgrid = FALSE),
+                       xaxis = list(title = "Year"))
+        } else {
+            gci_top_10_category %>%
+                plot_ly(x = ~fct_rev(Year), y = ~Rank, color = ~Country,
+                        type = "scatter", mode = "lines+markers",
+                        line = list(width = 3), marker = list(size = 10),
+                        colors = distinctColorPalette(nlevels(gci_top_10_category$Country)),
+                        hoverinfo = "text",
+                        text = ~paste("Country:", Country, "<br>Year:", Year, "<br>Rank:", Rank)) %>%
+                layout(yaxis = list(autorange = "reversed", tickmode = "linear", showgrid = FALSE),
+                       xaxis = list(title = "Year"))
+        }
+    })
     
     output$plot1 <- renderPlot({
         data <- histdata[seq_len(input$slider)]
@@ -133,8 +185,3 @@ server <- function(input, output) {
 }
 
 shinyApp(ui, server)
-
-
-
-
-
